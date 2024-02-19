423c9a21931286c87004873d846eb79da4e8604d
Keith E <keith.everitt@sonarsource.com>
2024-02-19 14:23:47 -0600

    Adding back HW.java with Errors

---
+++ src/main/java/com/acme/basic/HelloWorld.java
@@ -0,0 +1,45 @@
+package com.acme.basic;
+
+public class HelloWorld {
+
+  void sayHello() {
+    System.out.println("Hello World!");
+  }
+
+  void notCovered() {
+    System.out.println("This method is not covered by unit tests");
+  }
+
+  void keithTest(){
+    System.out.println("Write a log damnit!");
+  }
+
+  void keithTest12(){
+    System.out.println("Write a log damnit!");
+  }
+
+  void keithTest2(){
+    System.out.println("Write a log damnit!");
+  }
+
+  void keithTest3(){
+    System.out.println("Write a log damnit!");
+  }
+
+  void keithTest4(){
+    System.out.println("Write a log damnit!");
+  }
+
+  void keithTest5(){
+    System.out.println("Write a log damnit!");
+  }
+
+  void keithTest6(){
+    System.out.println("Write a log damnit!");
+  }
+
+  void keithTest7(){
+    System.out.println("Write a log damnit!");
+  }
+
+}
