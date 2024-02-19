c1b3990cc7288539bc2e120312cb59463fc79ccc
Keith E <keith.everitt@sonarsource.com>
2024-02-19 13:53:56 -0600

    adding another notCovered

--- src/main/java/com/acme/basic/HelloWorld.java
+++ src/main/java/com/acme/basic/HelloWorld.java
@@ -10,4 +10,8 @@ public class HelloWorld {
     System.out.println("This method is not covered by unit tests");
   }
 
+  void notCovered() {
+    System.out.println("This method is not covered by unit tests");
+  }
+
 }
