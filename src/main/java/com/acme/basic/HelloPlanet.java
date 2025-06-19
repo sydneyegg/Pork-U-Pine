899a95a5142e8304f6de00204817985d91f92852
sydneyegg <sydney.eggleston@sonarsource.com>
2025-06-19 10:20:42 -0500

    Update HelloPlanet.java
    added say hi function

--- src/main/java/com/acme/basic/HelloPlanet.java
+++ src/main/java/com/acme/basic/HelloPlanet.java
@@ -6,6 +6,10 @@ public class HelloPlanet {
     System.out.println("Hello Planet!");
   }
 
+    void sayHi() {
+    System.out.println("Hello Sydney!");
+  }
+
   void notCovered() {
     System.out.println("This method is not covered by unit tests");
   }
